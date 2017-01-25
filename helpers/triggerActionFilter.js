443f84a6d4c4f68c6707900695c5f562018c71cf
Adrien Antoine <adriantoine@gmail.com>
2017-01-25 12:17:50 +0000

    Added an additional check for the length of the array in the filter helper

--- helpers/triggerActionFilter.js
+++ helpers/triggerActionFilter.js
@@ -1,5 +1,5 @@
 module.exports = function (params, http) {
-  if(params.action && _.isArray(params.action)) {
+  if(params.action && _.isArray(params.action) && params.action.length > 0) {
     return params.action.indexOf(http.body.action) !== -1;
   }
 
